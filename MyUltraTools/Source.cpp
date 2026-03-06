f6140b5e3bb85b5a3596e200b2a0efe5b03ebb37
Mihail Tumanov <tumanov.mike@yandex.ru>
2026-03-07 03:44:21 +0700

    WIP - DON'T DRAW TRIANGLE

--- MyUltraTools/Source.cpp
+++ MyUltraTools/Source.cpp
@@ -8,18 +8,28 @@
 #include <d3dx10.h>
 #include <xnamath.h>
 
+
+
 LPCSTR WndClassName = "3D REDACTOR";
 HWND hWND = NULL;
 
 const int Wight = 800;
 const int Heignt = 800;
 
-// new_
-//1
 IDXGISwapChain* SwapChain;
 ID3D11Device* d3d11Device;
 ID3D11DeviceContext* d3d11DevCon;
 ID3D11RenderTargetView* renderTargetView;
+HRESULT hr;
+
+//new_
+ID3D11Buffer* triangleVertBuffer;
+ID3D11VertexShader* VS;
+ID3D11PixelShader* PS;
+ID3D10Blob* VS_Buffer;
+ID3D10Blob* PS_Buffer;
+ID3D11InputLayout* VertLayout;
+//new-
 
 float red = 0.0f;
 float green = 0.0f;
@@ -27,9 +37,31 @@ float blue = 0.0f;
 int colormodr = 1;
 int colormodg = 1;
 int colormodb = 1;
-//2
+
+//new_
+struct Vertex
+{
+	Vertex() {};
+	Vertex(float x, float y, float z) : pos(x, y, z) {}
+
+	XMFLOAT3 pos;
+};
+
+void InitLayout()
+{
+	//ID3D11Device::CreateInputLayout;
+	//ID3D11Device::CreateBuffer;
+	//ID3D11DeviceContext::IAGetVertexBuffers;
+	//ID3D11DeviceContext::IASetInputLayout;
+	//ID3D11DeviceContext::IASetPrimitiveTopology;
+	//ID3D11DeviceContext::Draw;
+	//ID3D11DeviceContext::RSSetViewports;
+	//ID3D11DeviceContext::OMSetRenderTargets;
+	//IDXGISwapChain::Present;
+}
+//new-
+
 bool InitializeDirect3dApp(HINSTANCE hInstance) {
-	HRESULT hr;
 
 	DXGI_MODE_DESC bufferDesc;
 	ZeroMemory(&bufferDesc, sizeof(DXGI_MODE_DESC));
@@ -68,13 +100,84 @@ bool InitializeDirect3dApp(HINSTANCE hInstance) {
 	return true;
 };
 
-void RealeaseObject() {
+void CleanUp() {
 	SwapChain->Release();
 	d3d11Device->Release();
 	d3d11DevCon->Release();
+	triangleVertBuffer->Release();
+	VS->Release();
+	PS->Release();
+	VS_Buffer->Release();
+	PS_Buffer->Release();
+	VertLayout->Release();
 };
 
-bool InitScene() { return true; };
+bool InitScene() { 
+	
+	D3D11_INPUT_ELEMENT_DESC layout[] =
+	{
+		{"POSITION",0,DXGI_FORMAT_R32G32B32A32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0},
+	};
+	UINT numElement = ARRAYSIZE(layout);
+
+	hr = D3DX11CompileFromFileA("vs_ef.fx", 0, 0, "VS", "vs_5_0", 0, 0, 0, &VS_Buffer, 0, 0);
+	hr = D3DX11CompileFromFileA("ps_ef.fx", 0, 0, "PS", "ps_5_0", 0, 0, 0, &PS_Buffer, 0, 0);
+	
+	d3d11Device->CreateVertexShader(VS_Buffer->GetBufferPointer(), VS_Buffer->GetBufferSize(), NULL, &VS);
+	d3d11Device->CreatePixelShader (PS_Buffer->GetBufferPointer(), PS_Buffer->GetBufferSize(), NULL, &PS);
+
+	d3d11DevCon->VSSetShader(VS, 0, 0);
+	d3d11DevCon->PSSetShader(PS, 0, 0);
+	
+
+	Vertex v[] =
+	{
+		Vertex( 0.0f,  0.5f,  0.5f),
+		Vertex( 0.5f, -0.5f,  0.5f),
+		Vertex(-0.5f, -0.5f,  0.5f),
+	};
+	
+
+	D3D11_BUFFER_DESC vertexBufferDecs;
+	ZeroMemory(&vertexBufferDecs, sizeof(D3D11_BUFFER_DESC));
+	vertexBufferDecs.Usage = D3D11_USAGE_DEFAULT;
+	vertexBufferDecs.ByteWidth = sizeof(Vertex) * 3;
+	vertexBufferDecs.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertexBufferDecs.CPUAccessFlags = 0;
+	vertexBufferDecs.MiscFlags = 0;
+
+	D3D11_SUBRESOURCE_DATA vertexBufferData;
+	ZeroMemory(&vertexBufferData, sizeof(D3D11_SUBRESOURCE_DATA));
+	vertexBufferData.pSysMem = v;
+
+
+	hr = d3d11Device->CreateBuffer(&vertexBufferDecs, &vertexBufferData, &triangleVertBuffer);
+
+
+	UINT stride = sizeof(Vertex);
+	UINT offset = 0;
+	d3d11DevCon->IAGetVertexBuffers(0, 1, &triangleVertBuffer, &stride, &offset);
+
+
+	hr = d3d11Device->CreateInputLayout(layout, numElement, VS_Buffer->GetBufferPointer(), VS_Buffer->GetBufferSize(), &VertLayout);
+	d3d11DevCon->IASetInputLayout(VertLayout);
+
+
+	d3d11DevCon->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+
+	D3D11_VIEWPORT viewport;
+	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = Wight;
+	viewport.Height = Heignt;
+
+
+	d3d11DevCon->RSSetViewports(1,&viewport);
+
+	return true; 
+};
 
 void UpdateScene() {
 		red += colormodr * 0.00005f;
@@ -90,14 +193,14 @@ void UpdateScene() {
 };
 
 void DrawScene() {
-	D3DXCOLOR bgColor(red, green, blue, 1.0f);
+	D3DXCOLOR bgColor(red, 0.0f, 0.0f, 0.0f);
 
 	d3d11DevCon->ClearRenderTargetView(renderTargetView, bgColor);
 
+	d3d11DevCon->Draw(3,0);
+
 	SwapChain->Present(0, 0);
 };
-//3
-// new-
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
@@ -180,6 +283,7 @@ int MassegeLoop() {
 		}
 		else
 		{
+			
 			UpdateScene();
 			DrawScene();
 
@@ -209,7 +313,7 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE rhinstance, LPSTR lpstr, int n
 	}
 
 	MassegeLoop();
-	RealeaseObject();
+	CleanUp();
 
 	return 0;
 }
---
+++ MyUltraTools/vs_ef.fx
@@ -0,0 +1,5 @@
+float4 VS(float4 inPos : POSITION) : SV_Position
+{
+    
+    return inPos;
+}
